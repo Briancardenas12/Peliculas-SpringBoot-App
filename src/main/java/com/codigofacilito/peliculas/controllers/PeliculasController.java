d9d3f6bd61f9fe9c91c02db17a494150d447c0d8
Briancardenas12 <social.bri009@gmail.com>
2025-12-06 20:41:18 -0500

    Second  commit: Spring Boot Peliculas App - Controller y Services faltantes

--- src/main/java/com/codigofacilito/peliculas/controllers/PeliculasController.java
+++ src/main/java/com/codigofacilito/peliculas/controllers/PeliculasController.java
@@ -3,17 +3,17 @@ package com.codigofacilito.peliculas.controllers;
 import com.codigofacilito.peliculas.entities.Actor;
 import com.codigofacilito.peliculas.entities.Pelicula;
 import com.codigofacilito.peliculas.services.IActorService;
+import com.codigofacilito.peliculas.services.IArchivoService;
 import com.codigofacilito.peliculas.services.IGeneroService;
 import com.codigofacilito.peliculas.services.IPeliculaService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,12 +24,14 @@ public class PeliculasController {
     private IPeliculaService service;
     private IGeneroService generoService;
     private IActorService actorService;
+    private IArchivoService archivoService;
 
 
-    public PeliculasController(IPeliculaService service, IGeneroService generoService, IActorService actorService){
+    public PeliculasController(IPeliculaService service, IGeneroService generoService, IActorService actorService, IArchivoService archivoService){
         this.service = service;
         this.generoService = generoService;
         this.actorService = actorService;
+        this.archivoService = archivoService;
     }
 
     @GetMapping("pelicula")
@@ -53,13 +55,27 @@ public class PeliculasController {
     }
 
     @PostMapping("/pelicula")
-    public String guardar(@Valid Pelicula pelicula, BindingResult br, @ModelAttribute(name = "ids") String ids, Model model){
+    public String guardar(@Valid Pelicula pelicula, BindingResult br, @ModelAttribute(name = "ids") String ids, Model model, @RequestParam("archivo") MultipartFile imagen){
 
         if (br.hasErrors()) {
             model.addAttribute("generos", generoService.findAll());
             model.addAttribute("actores", actorService.findAll());
             return "pelicula";
         }
+
+
+        if (!imagen.isEmpty()) {
+            String archivo = pelicula.getNombre() + getExtension(imagen.getOriginalFilename());
+            pelicula.setImagen(archivo);
+            try {
+                archivoService.guardar(archivo, imagen.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            pelicula.setImagen("_default.jpg");
+        }
+
 
         List<Long> idsProtaginistas = Arrays.stream(ids.split(",")).map(Long::parseLong).collect(Collectors.toList());
         List<Actor> protaginistas = actorService.findAllById(idsProtaginistas);
@@ -68,9 +84,14 @@ public class PeliculasController {
         return "redirect:home";
     }
 
+    private String getExtension(String archivo) {
+        return archivo.substring(archivo.lastIndexOf("."));
+    }
+
     @GetMapping({"/", "/home", "/index"})
     public String home(Model model){
 
+        model.addAttribute("peliculas", service.findAll());
         model.addAttribute("msj", "Catalogo Brian");
         model.addAttribute("tipoMsj", "success");
         return "home";
